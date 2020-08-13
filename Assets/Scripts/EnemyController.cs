e1b75c619a5d12d421e4b168a3bba636cda8fed7
Ryuuramaru <rosumateisc@gmail.com>
2020-08-14 01:41:23 +0300

    AI WORKS
    GENIUS MOVE

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,26 +10,23 @@ public class EnemyController : MonoBehaviour
     Transform target;
     NavMeshAgent agent;
     
-    // Start is called before the first frame update
+
     void Start()
     {
         target = PlayerManager.instance.player.transform;
+
         agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
 
         if(distance <= lookRadius)
         {
-            agent.SetDestination(target.position);
+            if(distance >= agent.stoppingDistance + 3) agent.SetDestination(target.position); //no jiggering i swear
 
-            if (distance <= agent.stoppingDistance)
-            {
-                FaceTarget();
-            }
+            FaceTarget(); //look into my eyes
         }
     }
 
